9c484ce7f50ac96f87d98d0a2cfbf5d7f8b28660
pompill <908902377@qq.com>
2018-01-30 00:20:25 +0800

    lie_pin_twice_modify

--- LiePin/spiders/LiePin/utils/changeMs.py
+++
@@ -1,9 +0,0 @@
-import time
-
-
-def change_ms(d):
-    if len(d) == 10:
-        date = time.mktime(time.strptime(d, '%Y-%m-%d')) * 1000
-    else:
-        date = time.mktime(time.strptime(d, '%Y-%m-%d %H:%M:%S')) * 1000
-    return int(date)
